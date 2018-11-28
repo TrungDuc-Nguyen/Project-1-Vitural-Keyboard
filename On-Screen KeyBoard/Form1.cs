ac2a8854db2f1c1a3b8aa5a79f0fda174e351f03
TrungDuc-Nguyen <nguyentrungduc.bka@gmail.com>
2018-11-28 23:36:48 +0700

    TrungDuc-Nguyen

--- On-Screen KeyBoard/Form1.cs
+++ On-Screen KeyBoard/Form1.cs
@@ -238,6 +238,26 @@ namespace On_Screen_KeyBoard
                     return "\\";
                 case Keys.NumPad3:
                     return "'";
+                case Keys.D1:
+                    return "1";
+                case Keys.D2:
+                    return "2";
+                case Keys.D3:
+                    return "3";
+                case Keys.D4:
+                    return "4";
+                case Keys.D5:
+                    return "5";
+                case Keys.D6:
+                    return "6";
+                case Keys.D7:
+                    return "7";
+                case Keys.D8:
+                    return "8";
+                case Keys.D9:
+                    return "9";
+                case Keys.D0:
+                    return "0";
                 default:
                     return key.ToString();
 #endregion
